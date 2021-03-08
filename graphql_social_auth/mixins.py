745fa023284e758aeafa42f91ea1ebc2f1a97cbf
Mohit Solanki <68371874+mohit-deepsource@users.noreply.github.com>
2021-03-08 14:30:34 +0530

    Update mixins.py (#1)

--- graphql_social_auth/mixins.py
+++ graphql_social_auth/mixins.py
@@ -1,4 +1,11 @@
 import graphene
+from calendar import timegm
+from datetime import datetime
+
+import graphene
+import graphql_jwt
+from django.utils import timezone
+from graphql_jwt.settings import jwt_settings
 
 
 class SocialAuthMixin:
@@ -21,6 +28,29 @@ class ResolveMixin:
 
 class JSONWebTokenMixin:
     token = graphene.String()
+    refresh_token = graphene.String()
+    token_expires_in = graphene.Int()
+    refresh_expires_in = graphene.Int()
+
+    @staticmethod
+    def resolve_refresh_token(social_auth, info):
+        refresh_token = graphql_jwt.refresh_token.shortcuts.create_refresh_token(
+            social_auth.social.user
+        )
+        info.context.jwt_refresh_token = refresh_token
+        return refresh_token.get_token()
+
+    @staticmethod
+    def resolve_refresh_expires_in(*_args, **_kwargs):
+        refresh_expires_in = (
+            timegm(datetime.utcnow().utctimetuple())
+            + jwt_settings.JWT_REFRESH_EXPIRATION_DELTA.total_seconds()
+        )
+        return refresh_expires_in
+
+    @staticmethod
+    def resolve_token_expires_in(*_args, **_kwargs):
+        return int((timezone.now() + jwt_settings.JWT_EXPIRATION_DELTA).timestamp())
 
     @classmethod
     def resolve(cls, root, info, social, **kwargs):
