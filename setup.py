591962a288b6fdb2427203dacc6de9631b4edcc3
Eshaan Bansal <eshaan@deepsource.io>
2023-10-03 15:27:34 +0530

    chore: use upstream version for django-graphql-jwt
    Signed-off-by: Eshaan Bansal <eshaan@deepsource.io>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'django-filter>=1.1.0',
         'graphene-django>=2.0.0',
         'social-auth-app-django>=2.1.0',
-        'django-graphql-jwt @ git+https://git@github.com/deepsourcelabs/django-graphql-jwt.git@master',
+        'django-graphql-jwt==0.4.0',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
